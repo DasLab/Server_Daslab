104bb143351ed7dce15f0fad086500dcd112fff0
T47 <t47@stanford.edu>
2015-06-18 16:49:56 -0700

    try static_root

--- src/settings.py
+++ src/settings.py
@@ -29,8 +29,8 @@ MEDIA_URL = ''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = ''
-STATICFILES_DIRS = (MEDIA_ROOT + '/data', MEDIA_ROOT+'/media')
+STATIC_ROOT = MEDIA_ROOT+'/media'
+# STATICFILES_DIRS = (MEDIA_ROOT + '/data', MEDIA_ROOT+'/media')
 ADMIN_MEDIA_PREFIX = '/admin/'
 
 ADMINS = (
