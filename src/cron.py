c3221965e6dd1208009d407834ea297a602feda3
Ubuntu <ubuntu@daslab.stanford.edu>
2015-07-03 16:53:53 +0000

    change .py chmod

--- src/cron.py
+++ src/cron.py
@@ -5,7 +5,7 @@ from src.settings import MEDIA_ROOT
 
 def backup_weekly():
 	print "!cron!"
-	os.popen('touch ~/%s.txt' % datetime.now().strftime('%H-%M-%S'))
+	os.popen('touch /home/ubuntu/%s.txt' % datetime.now().strftime('%H-%M-%S'))
 	# os.popen('cd %s && python util_backup.py' % MEDIA_ROOT)
 
 
