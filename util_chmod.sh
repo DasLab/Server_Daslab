5c04cc9e6ab042134654153682203baa542aa3a2
T47 <t47@stanford.edu>
2015-07-08 21:26:28 -0700

    relax robots.txt

--- util_chmod.sh
+++ util_chmod.sh
@@ -7,14 +7,13 @@ sudo chown -R ubuntu *.sh
 sudo chmod -R 700 *.sh
 
 sudo chmod 640 *.py* *.md *.txt
-sudo chmod 440 robots.txt
 sudo chmod 640 src/*.py* src/templatetags/*
 sudo chmod 750 src src/templatetags
 sudo chmod 640 media/css/* media/fonts/* media/html/* media/js/* media/_old/* media/swf/*
 sudo chmod 750 media/css media/fonts media/html media/js media/_old media/swf media
-sudo chmod 640 media/images/*.jpg media/images/*.png media/images/contact/* media/images/group/* media/images/home/* media/images/news/* media/images/people/* media/images/publications/* media/images/research/* media/images/resources/*
+sudo chmod 640 media/images/*.*g* media/images/contact/* media/images/group/* media/images/home/* media/images/news/* media/images/people/* media/images/publications/* media/images/research/* media/images/resources/*
 sudo chmod 750 media/images media/images/contact media/images/group media/images/home media/images/news media/images/people media/images/publications media/images/research media/images/resources
-sudo chmod 640 media/admin/*.html media/admin/css/* media/admin/img/*.gif media/admin/img/*.png media/admin/img/gis/* media/admin/img/filemanager/* media/admin/js/*.js media/admin/js/custom/* media/admin/js/filemanager/* media/admin/js/suit/*
+sudo chmod 640 media/admin/*.html media/admin/css/* media/admin/img/*.*g* media/admin/img/gis/* media/admin/img/filemanager/* media/admin/js/*.js media/admin/js/custom/* media/admin/js/filemanager/* media/admin/js/suit/*
 sudo chmod 750 media/admin media/admin/css media/admin/img media/admin/img/gis media/admin/img/filemanager media/admin/js media/admin/js/custom media/admin/js/filemanager media/admin/js/suit
 
 sudo chmod 640 backup/*
