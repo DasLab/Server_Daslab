06db6ed166edc492c6765563813461a6aa4c2ff5
T47 <t47@stanford.edu>
2015-07-22 17:39:52 -0700

    fix apache traffic parse

--- media/admin/js/custom/apache.js
+++ media/admin/js/custom/apache.js
@@ -5,7 +5,7 @@ function get_apache_stat() {
         url : "/admin/apache_stat",
         dataType: "json",
         success : function (data) {
-            console.log(data);
+            // console.log(data);
 
             $("#apache_title").html(data['title']);
             $("#apache_ver").html(data['ver_apache']);
@@ -23,8 +23,9 @@ function get_apache_stat() {
             $("#apache_server_load").html('<span style="color:#f00;">' + server_load[0] + '</span> | <span style="color:#080;">' + server_load[1] + '</span> | <span style="color:#00f;">' + server_load[2] + '</span>');
             var cpu_usage = data['cpu_usage'].split(/\//);
             $("#apache_cpu_usage").html('<i>u</i> <span style="color:#f00;">' + cpu_usage[0] + '</span> | <i>s</i> <span style="color:#080;">' + cpu_usage[1] + '</span> | <i>cu</i> <span style="color:#00f;">' + cpu_usage[2] + '</span> | <i>cs</i> <span style="color:#f80;">' + cpu_usage[3] + '</span>');
-            var traffic = data['traffic'].split(/\//);
-            $("#apache_traffic").html('<span style="color:#f00;">' + traffic[0] + '</span> <i>req/s</i> | <span style="color:#080;">' + traffic[1] + '</span> <i>B/s</i> | <span style="color:#00f;">' + traffic[2] + '</span> <i>kB/req</i>');
+            var traffic = data['traffic'].split(/\//), traffic_req_unit = '';
+            if (traffic.length == 4) {traffic_req_unit = traffic[3]}
+            $("#apache_traffic").html('<span style="color:#f00;">' + traffic[0] + '</span> <i>req/s</i> | <span style="color:#080;">' + traffic[1] + '</span> <i>B/s</i> | <span style="color:#00f;">' + traffic[2] + '</span> <i>' + traffic_req_unit + 'B/req</i>');
             $("#apache_total_access").html(data['total_access']);
             $("#apache_total_traffic").html('<span style="color:#00f">' + data['total_traffic'] + '</span>');
             $("#apache_cpu_load").html(data['cpu_load'] + '%');
