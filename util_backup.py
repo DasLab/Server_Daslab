534437194e73f686ee123f9548fafc0146e849fe
T47 <t47@stanford.edu>
2015-07-28 17:39:59 -0700

    ditch os.popen for utils, add exit code and error report

--- util_backup.py
+++ util_backup.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 sys.path.append(os.path.abspath('.'))
@@ -6,18 +7,41 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
 from src.settings import *
 
+
+flag = False
 print "#1: Backing up MySQL database..."
-os.popen('mysqldump --quick daslab -u root -pbeckman | gzip > %s/backup/backup_mysql.gz' % MEDIA_ROOT)
-print "    \033[92mSUCCESS\033[0m: \033[94mMySQL\033[0m database dumped."
+try:
+	subprocess.check_call('mysqldump --quick %s -u %s -p%s > %s/backup/backup_mysql' % (env.db()['NAME'], env.db()['USER'], env.db()['PASSWORD'], MEDIA_ROOT), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	subprocess.check_call('gzip -f %s/backup/backup_mysql' % MEDIA_ROOT, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError:
+	print "    \033[41mERROR\033[0m: Failed to dump \033[94mMySQL\033[0m database."
+	flag = True
+else:
+	print "    \033[92mSUCCESS\033[0m: \033[94mMySQL\033[0m database dumped."
 
 print "#2: Backing up static files..."
-os.popen('cd %s && tar zcf backup/backup_static.tgz data/' % MEDIA_ROOT)
-print "    \033[92mSUCCESS\033[0m: \033[94mstatic\033[0m files synced."
+try:
+	subprocess.check_call('cd %s && tar zcf backup/backup_static.tgz data/' % MEDIA_ROOT, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError:
+	print "    \033[41mERROR\033[0m: Failed to archive \033[94mstatic\033[0m files."
+	flag = True
+else:
+	print "    \033[92mSUCCESS\033[0m: \033[94mstatic\033[0m files synced."
 
 print "#3: Backing up apache2 settings..."
-os.popen('cp -r /etc/apache2 %s/backup' % MEDIA_ROOT)
-os.popen('cd %s/backup && tar zcf backup_apache.tgz apache2/' % MEDIA_ROOT)
-os.popen('rm -rf %s/backup/apache2' % MEDIA_ROOT)
-print "    \033[92mSUCCESS\033[0m: \033[94mapache2\033[0m settings saved."
+try:
+	subprocess.check_call('cp -r /etc/apache2 %s/backup' % MEDIA_ROOT, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	subprocess.check_call('cd %s/backup && tar zcf backup_apache.tgz apache2/' % MEDIA_ROOT, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	subprocess.check_call('rm -rf %s/backup/apache2' % MEDIA_ROOT, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError:
+	print "    \033[41mERROR\033[0m: Failed to archive \033[94mapache2\033[0m settings."
+	flag = True
+else:
+	print "    \033[92mSUCCESS\033[0m: \033[94mapache2\033[0m settings saved."
 
-print "All done!"
+if flag:
+	print "Finished with errors!"
+	sys.exit(1)
+else:
+	print "All done successfully!"
+	sys.exit(0)
--- util_gdrive_sync.py
+++ util_gdrive_sync.py
@@ -8,6 +8,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
 from src.settings import *
 
+
+flag = False
+
 t = datetime.datetime.now().strftime('%Y%m%d')
 gdrive_dir = 'echo'
 if not DEBUG: gdrive_dir = 'cd %s' % APACHE_ROOT
@@ -15,20 +18,34 @@ prefix = ''
 if DEBUG: prefix = '_DEBUG'
 
 print "#1: Uploading MySQL database..."
-os.popen('%s && drive upload -f %s/backup/backup_mysql.gz -t DasLab_%s_mysql%s.gz' % (gdrive_dir, MEDIA_ROOT, t, prefix))
-print "    \033[92mSUCCESS\033[0m: \033[94mMySQL\033[0m database uploaded."
+try:
+	subprocess.check_call('%s && drive upload -f %s/backup/backup_mysql.gz -t DasLab_%s_mysql%s.gz' % (gdrive_dir, MEDIA_ROOT, t, prefix), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError:
+	print "    \033[41mERROR\033[0m: Failed to upload \033[94mMySQL\033[0m database."
+	flag = True
+else:
+	print "    \033[92mSUCCESS\033[0m: \033[94mMySQL\033[0m database uploaded."
 
 print "#2: Uploading static files..."
-os.popen('%s && drive upload -f %s/backup/backup_static.tgz -t DasLab_%s_static%s.tgz' % (gdrive_dir, MEDIA_ROOT, t, prefix))
-print "    \033[92mSUCCESS\033[0m: \033[94mstatic\033[0m files uploaded."
+try:
+	subprocess.check_call('%s && drive upload -f %s/backup/backup_static.tgz -t DasLab_%s_static%s.tgz' % (gdrive_dir, MEDIA_ROOT, t, prefix), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError:
+	print "    \033[41mERROR\033[0m: Failed to upload \033[94mstatic\033[0m files."
+	flag = True
+else:
+	print "    \033[92mSUCCESS\033[0m: \033[94mstatic\033[0m files uploaded."
 
 print "#3: Uploading apache2 settings..."
-os.popen('%s && drive upload -f %s/backup/backup_apache.tgz -t DasLab_%s_apache%s.tgz' % (gdrive_dir, MEDIA_ROOT, t, prefix))
-print "    \033[92mSUCCESS\033[0m: \033[94mapache2\033[0m settings uploaded."
+try:
+	subprocess.check_call('%s && drive upload -f %s/backup/backup_apache.tgz -t DasLab_%s_apache%s.tgz' % (gdrive_dir, MEDIA_ROOT, t, prefix), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+except subprocess.CalledProcessError:
+	print "    \033[41mERROR\033[0m: Failed to upload \033[94mapache2\033[0m settings."
+	flag = True
+else:
+	print "    \033[92mSUCCESS\033[0m: \033[94mapache2\033[0m settings uploaded."
 
 
 print "#4: Removing obsolete backups..."
-
 old = (datetime.date.today() - datetime.timedelta(days=KEEP_BACKUP)).strftime('%Y-%m-%dT00:00:00')
 
 list_mysql = subprocess.Popen("%s && drive list -q \"title contains 'DasLab' and (title contains '_mysql.gz' or title contains '_mysql_DEBUG.gz') and modifiedDate <= '%s'\"| awk '{ printf $1\" \"}'" % (gdrive_dir, old), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).communicate()[0].strip().split()[1:]
@@ -37,11 +54,21 @@ list_apache = subprocess.Popen("%s && drive list -q \"title contains 'DasLab' an
 list_all = list_mysql + list_static + list_apache
 
 for id in list_all:
-	os.popen('%s && drive info -i %s' % (gdrive_dir, id))
-	print
-	os.popen('%s && drive delete -i %s' % (gdrive_dir, id))
+	try:
+		subprocess.check_call('%s && drive info -i %s' % (gdrive_dir, id), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		print
+		subprocess.check_call('%s && drive delete -i %s' % (gdrive_dir, id), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	except subprocess.CalledProcessError:
+		print "    \033[41mERROR\033[0m: Failed to remove obsolete \033[94mbackup\033[0m files."
+		flag = True
 
-print "    \033[92mSUCCESS\033[0m: \033[94m%s\033[0m obsolete backup files removed." % len(list_all)
+if not flag: print "    \033[92mSUCCESS\033[0m: \033[94m%s\033[0m obsolete backup files removed." % len(list_all)
 
 
-print "All done!"
+if flag:
+	print "Finished with errors!"
+	sys.exit(1)
+else:
+	print "All done successfully!"
+	sys.exit(0)
+
