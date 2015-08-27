2493ccaed1a3ade6899c06eddec189a3dee35132
T47 <t47@stanford.edu>
2015-08-27 13:03:53 -0700

    remove sticky bits for folders

--- util_chmod.sh
+++ util_chmod.sh
@@ -6,22 +6,22 @@ sudo chown -R www-data backup data
 
 sudo chmod 640 *.py* *.md *.txt .gitignore
 sudo chmod 640 src/*.py* src/templatetags/*
-sudo chmod 1750 src src/templatetags
+sudo chmod 750 src src/templatetags
 sudo chmod 640 media/css/* media/fonts/* media/html/* media/fonts/GillSans/* media/js/* media/js/admin/* media/js/suit/* media/_old/* media/swf/*
-sudo chmod 1750 media/css media/fonts media/fonts/GillSans media/html media/js media/js/admin media/js/suit media/_old media/swf media
+sudo chmod 750 media/css media/fonts media/fonts/GillSans media/html media/js media/js/admin media/js/suit media/_old media/swf media
 sudo chmod 640 media/images/*.*g* media/images/contact/* media/images/group/* media/images/home/* media/images/news/* media/images/people/* media/images/publications/* media/images/research/* media/images/resources/*
-sudo chmod 1750 media/images media/images/contact media/images/group media/images/home media/images/news media/images/people media/images/publications media/images/research media/images/resources
+sudo chmod 750 media/images media/images/contact media/images/group media/images/home media/images/news media/images/people media/images/publications media/images/research media/images/resources
 sudo chmod 640 media/admin/*.html media/admin/img/*.*g* media/admin/img/gis/* media/admin/img/filemanager/* media/admin/js/*.js 
-sudo chmod 1750 media/admin media/admin/img media/admin/img/gis media/admin/img/filemanager media/admin/js 
+sudo chmod 750 media/admin media/admin/img media/admin/img/gis media/admin/img/filemanager media/admin/js 
 
 sudo chmod 640 backup/*
-sudo chmod 1750 backup
+sudo chmod 750 backup
 sudo chmod 640 data/*.txt data/*.log data/*.pdf data/news_img/* data/_old/* data/ppl_img/* data/pub_data/* data/pub_img/* data/pub_pdf/* data/rot_data/* data/rot_ppt/* data/spe_ppt/*
-sudo chmod 1750 data data/news_img data/_old data/ppl_img data/pub_data data/pub_img data/pub_pdf data/rot_data data/rot_ppt data/spe_ppt
+sudo chmod 750 data data/news_img data/_old data/ppl_img data/pub_data data/pub_img data/pub_pdf data/rot_data data/rot_ppt data/spe_ppt
 
 sudo chmod 640 config/*.py* config/*.example config/*.conf
 sudo chown www-data config/cron.conf 
-sudo chmod 1750 config
+sudo chmod 750 config
 
 sudo chgrp -R ubuntu *.sh
 sudo chown -R ubuntu *.sh
