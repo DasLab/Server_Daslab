d445a4eeca2b9f42da63c333f57ba1397ad9fd8c
T47 <t47@stanford.edu>
2015-09-14 21:55:33 -0700

    use live time for navbar

--- media/js/group/menu.js
+++ media/js/group/menu.js
@@ -1,6 +1,12 @@
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var weekNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
 setInterval(function () {
     var utc = new Date().toISOString().replace(/\..+/, '.000Z');
     $("#utc").html(utc);
+    var d = new Date(), ampm = (d.getHours() >= 12) ? "p.m." : "a.m.", hour = (d.getHours() >= 12) ? d.getHours() - 12 : d.getHours(), tz = d.toString().match(/\(([A-Za-z\s].*)\)/)[1];
+    $("#date").html(monthNames[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear() + ' (' + weekNames[d.getDay()] + ')');
+    $("#clock").html(hour + ':' + d.getMinutes() + ' ' + ampm + ' (' + tz + ')');
 }, 1000);
 
 
