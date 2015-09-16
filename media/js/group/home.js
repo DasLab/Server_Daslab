a29b54f5e2eef1dee06db95e07eabd1cd3038cb6
T47 <t47@stanford.edu>
2015-09-15 20:06:39 -0700

    fix navbar-collpase

--- media/js/group/home.js
+++ media/js/group/home.js
@@ -6,7 +6,7 @@ $.ajax({
 		for (var i = 0; i < data.users.length / 2; i++) {
             var presence = "<span>";
             if (data.users[i].presence) { presence = '<span style="color: #50cc32;"'; }
-			html += '<tr><td><img src="/site_media/images/icons/slack/></span></span></td><td><img src="' + data.users[i].image + '"/>&nbsp;&nbsp;<i>' + data.users[i].name + '</i></td></tr>';
+			html += '<tr><td><img src="/site_media/images/icons/slack.png"></span></span></td><td><img src="' + data.users[i].image + '"/>&nbsp;&nbsp;<i>' + data.users[i].name + '</i></td></tr>';
 		}
 		html += '<tr><td colspan="2" style="padding: 0px;"></td></tr>';
 		$("#table_slack_user").html(html);
