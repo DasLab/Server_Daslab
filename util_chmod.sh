02e62ba6777064240b875a4096f827ad32a0c430
T47 <t47@stanford.edu>
2015-09-23 10:20:31 -0700

    update sys_ver sh

--- util_chmod.sh
+++ util_chmod.sh
@@ -17,7 +17,7 @@ sudo chmod 750 media/admin media/admin/img media/admin/img/gis media/admin/img/f
 
 sudo chmod 640 backup/* #cache/* cache/aws/* cache/slack/* cache/git/* cache/dropbox/*
 sudo chmod 750 backup #cache cache/aws cache/slack cache/git cache/dropbox
-sudo chmod 640 data/*.txt data/*.log data/*.pdf data/news_img/* data/_old/* data/ppl_img/* data/pub_data/* data/pub_img/* data/pub_pdf/* data/rot_data/* data/rot_ppt/* data/spe_ppt/*
+sudo chmod 640 data/*.pdf data/news_img/* data/_old/* data/ppl_img/* data/pub_data/* data/pub_img/* data/pub_pdf/* data/rot_data/* data/rot_ppt/* data/spe_ppt/*
 sudo chmod 750 data data/news_img data/_old data/ppl_img data/pub_data data/pub_img data/pub_pdf data/rot_data data/rot_ppt data/spe_ppt
 
 sudo chmod 640 config/*.py* config/*.example config/*.conf
