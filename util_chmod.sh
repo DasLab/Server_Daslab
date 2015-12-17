5915fc77ed403f7845962eb9d35d681b8489d15f
tinecidy627 <t47@stanford.edu>
2015-12-17 23:08:12 +0000

    add sys asset to backup/config, simplify chmod

--- util_chmod.sh
+++ util_chmod.sh
@@ -1,32 +1,25 @@
 sudo usermod -a -G www-data ubuntu
 
 sudo chgrp -R www-data *
-# sudo chgrp -R ubuntu cache
 sudo chown -R ubuntu *.py *.md *.txt src media config .gitignore
 sudo chown -R www-data backup data cache
-
 sudo chmod 640 *.py* *.md *.txt .gitignore
-sudo chmod 640 src/*.py* src/management/* src/management/commands/* src/templatetags/*
-sudo chmod 750 src src/management src/management/commands src/templatetags
-sudo chmod 640 media/css/* media/fonts/* media/html/* media/fonts/GillSans/* media/fonts/Helvetica/* media/js/* media/js/admin/* media/js/group/* media/js/suit/* media/_old/* media/swf/*
-sudo chmod 750 media/css media/fonts media/fonts/GillSans media/fonts/Helvetica media/html media/js media/js/admin media/js/group media/js/suit media/_old media/swf media
-sudo chmod 640 media/images/*.*g* media/images/contact/* media/images/group/* media/images/group/clock/* media/images/group/baking/* media/images/home/* media/images/news/* media/images/people/* media/images/publications/* media/images/research/* media/images/resources/* media/images/icons/*
-sudo chmod 750 media/images media/images/contact media/images/group media/images/group/clock media/images/group/baking media/images/home media/images/news media/images/people media/images/publications media/images/research media/images/resources media/images/icons
-sudo chmod 640 media/admin/*.html media/admin/img/*.*g* media/admin/img/gis/* media/admin/img/filemanager/* media/admin/js/*.js 
-sudo chmod 750 media/admin media/admin/img media/admin/img/gis media/admin/img/filemanager media/admin/js 
 
-sudo chmod 640 media/css/min/* media/js/group/min/* media/js/admin/min/* media/js/suit/min/*
-sudo chmod 750 media/css/min media/js/group/min media/js/admin/min media/js/suit/min
+sudo chmod 640 $(find src -type f)
+sudo chmod 750 $(find src -type d)
+sudo chmod 640 $(find media -type f)
+sudo chmod 750 $(find media -type d)
 
-sudo chmod 660 backup/* cache/* cache/aws/* cache/slack/* cache/git/* cache/ga/* cache/dropbox/*
-sudo chmod 770 backup cache cache/aws cache/slack cache/git cache/ga cache/dropbox
-sudo chmod 660 data/*.pdf data/news_img/* data/_old/* data/ppl_img/* data/pub_data/* data/pub_img/* data/pub_pdf/* data/rot_data/* data/rot_ppt/* data/spe_ppt/*
-sudo chmod 770 data data/news_img data/_old data/ppl_img data/pub_data data/pub_img data/pub_pdf data/rot_data data/rot_ppt data/spe_ppt
+sudo chmod 660 $(find cache -type f)
+sudo chmod 770 $(find cache -type d)
+sudo chmod 660 $(find backup -type f)
+sudo chmod 770 $(find backup -type d)
+sudo chmod 660 $(find data -type f)
+sudo chmod 770 $(find data -type d)
 
-sudo chmod 640 config/*.py* config/*.example config/*.conf
+sudo chmod 640 $(find config -type f)
+sudo chmod 750 $(find config -type d)
 sudo chown www-data config/cron.conf config/bot.conf
-sudo chmod 750 config
 
-sudo chgrp -R ubuntu *.sh
-sudo chown -R ubuntu *.sh
+sudo chown -R ubuntu:ubuntu *.sh
 sudo chmod -R 700 *.sh
