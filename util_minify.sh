f4acc366d145829b4528a13ef9462c74688dc081
T47 <t47@stanford.edu>
2016-03-04 11:58:26 -0800

    concat js/css files for fewer requests

--- util_minify.sh
+++ util_minify.sh
@@ -16,3 +16,24 @@ rm media/css/*.min.min.css
 mv media/css/*.min.css media/css/min/
 mv media/css/min/bootstrap.min.css media/css/
 mv media/css/min/fullcalendar.min.css media/css/
+
+
+cat media/js/group/min/menu.min.js media/js/group/min/email.min.js > media/js/group/min/menu.js
+rm media/js/group/min/menu.min.js media/js/group/min/email.min.js
+mv media/js/group/min/menu.js media/js/group/min/menu.min.js
+
+cat media/js/group/min/home.min.js media/js/group/min/clock.min.js > media/js/group/min/home.js
+rm media/js/group/min/home.min.js media/js/group/min/clock.min.js
+mv media/js/group/min/home.js media/js/group/min/home.min.js
+
+cat media/js/suit/min/core.min.js media/js/suit/min/RelatedObjectLookups.min.js media/js/suit/min/jquery.init.min.js media/js/suit/min/actions.min.js media/js/suit/min/DateTimeShortcuts.min.js > media/js/suit/min/core.js
+rm media/js/suit/min/core.min.js media/js/suit/min/RelatedObjectLookups.min.js media/js/suit/min/jquery.init.min.js media/js/suit/min/actions.min.js media/js/suit/min/DateTimeShortcuts.min.js
+mv media/js/suit/min/core.js media/js/suit/min/core.min.js
+
+cat media/js/admin/min/_suit.min.js media/js/admin/min/table.min.js media/js/admin/min/clock.min.js media/js/admin/min/menu.min.js > media/js/admin/min/menu.js
+rm media/js/admin/min/_suit.min.js media/js/admin/min/table.min.js media/js/admin/min/clock.min.js media/js/admin/min/menu.min.js
+mv media/js/admin/min/menu.js media/js/admin/min/menu.min.js
+
+cat media/css/min/theme.min.css media/css/min/palette.min.css > media/css/min/theme.css
+rm media/css/min/theme.min.css media/css/min/palette.min.css
+mv media/css/min/theme.css media/css/min/theme.min.css
