e8e2ac079bc24ea4c7b0d4c0f44f3ec244522597
T47 <t47@stanford.edu>
2016-05-22 21:15:00 -0700

    use head.js for async load

--- media/js/admin/load.js
+++ media/js/admin/load.js
@@ -1,66 +1,24 @@
-yepnope({
-    'load': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/' + js_ver.jquery + '/jquery.min.js',
-    'complete': function() {
-        yepnope({
-            'test': window.jQuery,
-            'yep': [
-                'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/' + js_ver.bootstrap + '/js/bootstrap.min.js',
-                'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/' + js_ver.bootstrap + '/css/bootstrap.min.css'
-            ],
-            'nope': [
-                '/site_media/js/jquery.min.js',
-                '/site_media/js/bootstrap.min.js',
-                '/site_media/css/bootstrap.min.css'
-            ],
-            'callback': function() {
-                yepnope('/site_media/js/public/' + DEBUG_DIR + 'main' + DEBUG_STR + '.js');
-            }
+head.load('https://cdnjs.cloudflare.com/ajax/libs/jquery/' + js_ver.jquery + '/jquery.min.js', function() {
+    head.test(window.jQuery, [
+            'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/' + js_ver.bootstrap + '/css/bootstrap.min.css',
+            'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/' + js_ver.bootstrap + '/js/bootstrap.min.js'
+        ], [
+            '/site_media/js/jquery.min.js',
+            '/site_media/css/bootstrap.min.css',
+            '/site_media/js/bootstrap.min.js'
+        ], function() {
+            $.ajaxSetup({'cache': true});
+            $.getScript('/site_media/js/public/' + DEBUG_DIR + 'main' + DEBUG_STR + '.js');
+            $("head").append('<link rel="shortcut icon" href="/site_media/images/icon_daslab.png" />');
+            $("head").append('<link rel="icon" type-"image/gif" href="/site_media/images/icon_daslab.png" />');
         });
-    }
 });
 
 
-// var xmlhttp = new XMLHttpRequest(), isCDN = false;
-// var ver_jquery, ver_bootstrap, ver_swfobj, ver_fullcal, ver_moment, ver_dropz;
-// xmlhttp.onreadystatechange = function() {
-//     if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-//         if (xmlhttp.status == 200) {
-//             var xmlDoc = JSON.parse(xmlhttp.responseText);
-//             ver_jquery = xmlDoc.jquery;
-//             ver_bootstrap = xmlDoc.bootstrap;
-//             ver_swfobj = xmlDoc.swfobj;
-//             ver_fullcal= xmlDoc.fullcal;
-//             ver_moment = xmlDoc.moment;
-
-//             document.write('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/' + ver_jquery + '/jquery.min.js"><\/script>');
-//             document.write('\
-//                 <script type="text/javascript"> \
-//                     if (!window.jQuery) { \
-//                         document.write(\'<script type="text/javascript" src="/site_media/js/jquery.min.js"><\' + \'/script>\'); \
-//                         document.write(\'<script type="text/javascript" src="/site_media/js/bootstrap.min.js"><\' + \'/script>\'); \
-//                         document.write(\'<link rel="stylesheet" href="/site_media/css/bootstrap.min.css" \/>\'); \
-//                         isCDN = false; \
-//                     } else { \
-//                         document.write(\'<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/' + ver_bootstrap + '/js/bootstrap.min.js"><\' + \'/script>\'); \
-//                         document.write(\'<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/' + ver_bootstrap + '/css/bootstrap.min.css" \/>\'); \
-//                         isCDN = true; \
-//                     } \
-//                 \x3C\/script>');
-//         } else {
-//             document.write('<script type="text/javascript" src="/site_media/js/jquery.min.js"><\/script>');
-//             document.write('<script type="text/javascript" src="/site_media/js/bootstrap.min.js"><\/script>');
-//             document.write('<link rel="stylesheet" href="/site_media/css/bootstrap.min.css" \/>');
-//         }
 
 //         if (DEBUG_DIR) {
 //             document.write('<link rel="stylesheet" href="/site_media/css/' + DEBUG_DIR + 'theme' + DEBUG_STR + '.css" \/>');
 //         } else {
 //             document.write('<link rel="stylesheet" href="/site_media/css/theme.css" \/>');
 //             document.write('<link rel="stylesheet" href="/site_media/css/palette.css" \/>');
-//         }
-//         document.write('<link rel="shortcut icon" href="/site_media/images/icon_daslab.png" \/>');
-//         document.write('<link rel="icon" type-"image/gif" href="/site_media/images/icon_daslab.png" \/>');
-//     }
-// };
-// xmlhttp.open("GET", "/get_js/", false);
-// xmlhttp.send();
+
